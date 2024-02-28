48d9382ff84088655516c91c66c699179ac24fef
majikRuiz <137236354+majikhacker@users.noreply.github.com>
2024-02-27 20:53:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+chatbot_tone = {'role': 'system', 'content': 'you are a slightly sarcastic yet efficient assistant'}
+st.session_state.messages.append(system_message)
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Oh....it's you. What do you need?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
